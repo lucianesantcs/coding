a7c09428920c8f0a13f81bbde54da05028c2914f
Luciane Santos <lucianesantcs@gmail.com>
2020-10-30 23:15:26 -0300

    erick-wendel jsexpert aula 3

---
+++ erick-wendel/aula02/public/bandersnatch/src/videoComponent.js
@@ -0,0 +1,13 @@
+class VideoComponent {
+  constructor() {}
+  initializePlayer() {
+    const player = videojs('vid');
+    const ModalDialog = videojs.getComponent('ModalDialog');
+    const modal = new ModalDialog(player, {
+      temporary: false,
+      closeable: true,
+    });
+
+    player.addChild(modal);
+  }
+}
